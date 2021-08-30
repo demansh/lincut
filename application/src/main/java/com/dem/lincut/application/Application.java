b563b6b0e840c2f34e2c3edb88f866c124f2f8af
demansh <demansh17@gmail.com>
2021-08-30 09:30:46 +0500

    added scan packages

--- application/src/main/java/com/dem/lincut/application/Application.java
+++ application/src/main/java/com/dem/lincut/application/Application.java
@@ -3,7 +3,7 @@ package com.dem.lincut.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dem.lincut")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
