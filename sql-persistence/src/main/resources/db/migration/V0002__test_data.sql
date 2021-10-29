f0d17d8227d825158c19436cf5e1f1f71f62eef4
Aleksey Demshin <demansh17@gmail.com>
2021-10-29 09:19:48 +0500

    added test data to db

---
+++ sql-persistence/src/main/resources/db/migration/V0002__test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO link (url)
+VALUES ('https://yandex.ru'),
+    ('https://habrahabr.ru'),
+    ('https://ya.ru'),
+    ('https://vc.ru'),
+    ('https://github.com'),
+    ('https://docs.spring.io'),
+    ('https://miro.com'),
+    ('https://youtube.com'),
+    ('https://stackowerflow.com')
